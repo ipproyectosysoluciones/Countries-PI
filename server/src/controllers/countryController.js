aa2dcbdbd527ee92b2d0aef68b7bcf25597f15dc
Bladimir Parra - IP Proyectos y Soluciones <bladyparra@gmail.com>
2023-07-13 01:38:22 -0500

    Creacion de 'Routes', 'Controllers'  y 'Handlers'

---
+++ server/src/controllers/countryController.js
@@ -0,0 +1,48 @@
+const { Country } = require("../db.js");
+const { Activity } = require("../db.js");
+const { formatCountries } = require("../utils/generalFunctions.js");
+const { Op } = require("sequelize");
+
+const getCountryByName = async ( name ) => {
+  const country = await Country.findAll({
+    where: {
+      name: {
+        [ Op.iLike ]: `%${ name }%`,
+      },
+    },
+    include: {
+      model: Activity,
+      through: { attributes: [] },
+    },
+  });
+  return country;
+};
+
+const getAllCountries = async () => {
+  const countries = await Country.findAll({
+    include: {
+      model: Activity,
+      through: { attributes: [] },
+    },
+  });
+  return countries;
+};
+
+const getCountryById = async ( id ) => {
+  const country = await Country.findAll({
+    where: {
+      id: id.toUpperCase(),
+    },
+    include: {
+      model: Activity,
+      through: { attributes: [] },
+    },
+  });
+  return country;
+};
+
+module.exports = {
+  getCountryByName,
+  getAllCountries,
+  getCountryById,
+};
---
+++ server/src/routes/activitiesRouter.js
@@ -0,0 +1,13 @@
+const { Router } = require( "express" );
+const {
+  postActivity,
+  getActivities,
+  deleteActivity,
+} = require( "../handlers/activitiesHandlers" );
+
+const activitiesRouter = Router();
+activitiesRouter.post( "/", postActivity );
+activitiesRouter.get( "/", getActivities );
+activitiesRouter.delete( "/:idActivity", deleteActivity );
+
+module.exports = activitiesRouter;
