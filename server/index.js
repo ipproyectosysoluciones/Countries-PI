760c338d0676859b5dc15fbe86ab86ebe6215b7d
Bladimir Parra - IP Proyectos y Soluciones <bladyparra@gmail.com>
2023-07-13 01:39:32 -0500

    Update 'server'

--- server/index.js
+++ server/index.js
@@ -1,11 +1,31 @@
-const axios = require("axios");
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
 require( "dotenv" ).config();
-const PORT = 3001;
+const { PORT } = process.env;
 
-conn.sync({ force: true }).then(() => {
+// Syncing all the models at once.
+conn.sync({ force: false }).then(() => {
   server.listen( PORT, () => {
-    console.log( "%s listening at " + PORT );  // eslint-disable-line no-console
+    console.log( "%s listening at " + PORT ); // eslint-disable-line no-console
   });
-}).catch( error => console.error( error ) );
+});
--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 const express = require( "express" );
 const cookieParser = require( "cookie-parser" );
 const bodyParser = require( "body-parser" );
-const router = require( "./routes" );
+const routes = require( "./routes" );
 const morgan = require( "morgan" );
 const cors = require( "cors" );
 const { saveCountriesToDB } = require("./utils/api.js");
@@ -9,24 +9,18 @@ require( "./db.js" );
 
 const server = express();
 
+server.name = "API";
+
 server.use( bodyParser.urlencoded( { extended: true, limit: "50mb" } ) );
 server.use( bodyParser.json( { limit: "50mb" } ) );
 server.use( cookieParser() );
-server.use( morgan("dev") );
+server.use( morgan( "dev" ) );
 server.use( express.json() );
 server.use( cors() );
 
 saveCountriesToDB();
 
-server.use( router );
+server.use( "/", routes );
 
-// Error catching endware.
-server.use(( err, req, res, next ) => {
-  // eslint-disable-line no-unused-vars
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error( err );
-  res.status( status ).send(message);
-});
 
 module.exports = server;
