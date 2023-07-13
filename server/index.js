d1f43de31d9d6ca89ffee48af3914e7e569969f2
Bladimir Parra - IP Proyectos y Soluciones <bladyparra@gmail.com>
2023-07-13 00:18:35 -0500

    Creación de los modelos de la DB, creación del Server y Conexión con PostgreSql, Creación de 'utils'

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
+require( "dotenv" ).config();
 const PORT = 3001;
 
 conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-  })
-}).catch(error => console.error(error))
+  server.listen( PORT, () => {
+    console.log( "%s listening at " + PORT );  // eslint-disable-line no-console
+  });
+}).catch( error => console.error( error ) );
--- server/src/models/Country.js
+++ server/src/models/Country.js
@@ -3,9 +3,35 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Country', {
+  sequelize.define('country', {
+    id: {
+      type: DataTypes.STRING(3),
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
+    },
+    flags: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subregion: {
+      type: DataTypes.STRING,
+    },
+    area: {
+      type: DataTypes.INTEGER,
+    },
+    population: {
+      type: DataTypes.INTEGER,
       allowNull: false,
     },
   });
